053d15b85b3cd4e1a50c25b4a6bb72a25890eb7b
NicoleWrz <nicolew96@gmail.com>
2021-12-08 17:51:20 -0500

    route edits

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,12 +5,11 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 apiRoutes.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+
 });
 
 apiRoutes.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.json(`${req.method} request received to save notes`)
+
 });
 
 apiRoutes.delete("/notes", (req, res) => {
